f07825b879ff9e25dc86f28268a86e908fe9997f
fanabie <plnam@gmail.com>
2025-04-27 23:25:15 +0700

    Feat: create hash password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,30 +4,43 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { genSaltSync,hashSync} from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
 
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
+   getHashPass= (password : string) => {
+    const salt = genSaltSync(10);
+    const hash =  hashSync(password,salt);
+
+    return hash;
+  }
+
   async create(createUserDto: CreateUserDto) {
     let user = await this.userModel.create(createUserDto);
     return user;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    let users = await this.userModel.find();
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+ async findOne(id: number) {
+   let user = await this.userModel.findById(id);
+   return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+ async update(id: number, updateUserDto: UpdateUserDto) {
+    let user = await this.userModel.updateOne(UpdateUserDto);
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+ async remove(id: number) {
+   let user = await this.userModel.deleteOne();
+   return user;
+ }
+
 }
